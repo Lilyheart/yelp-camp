681094a3ae8dd04322cb4d39e7d363cefb3f3496
Lily Romano <lily.programs@gmail.com>
2016-06-21 23:22:44 +0000

    Colt Steele: Section 28 complete (74.9% complete)

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-// setup express & ejs
+// setup express, ejs, & mongoose
 
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose");
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// setup temporary array that will become a database later
+app.use(bodyParser.urlencoded({extended: true}));
 
-var campgrounds = [
-    {name: "Salmons Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-    {name: "Granite Hills", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
-    {name: "Mountain Goat", image: "https://farm6.staticflickr.com/5077/14477176044_4de99bd758.jpg"}
-];
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
 
 // setup routes
 
@@ -23,23 +28,43 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description:description};
     
-    campgrounds.push(newCampground);
-    
-    res.redirect("/campgrounds");
+    Campground.create(newCampground, function(err, newlyCreated){
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/index");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
 });
 
+app.get("/campgrounds/:id", function(req,res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", {campground: foundCampground});
+        }
+    });
+});
 
 
 // setup listener
